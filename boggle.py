76d0946288cec3b99d4eb9d65f9c557dfd9d3587
Ubuntu <ubuntu@ip-172-31-94-42.ec2.internal>
2019-09-26 14:43:24 +0000

    updated

--- boggle.py
+++ boggle.py
@@ -67,11 +67,15 @@ def search(grid, dictionary):
     """
     neighbours = all_grid_neighbours(grid)
     paths = []
+    full_words, stems = dictionary
+    
     
     def do_search(path):
         word = path_to_word(grid,path)
-        if word in dictionary:
+        if word in full_words:
             paths.append(path)
+        if word not in stems:
+            return 
         for next_pos in neighbours[path[-1]]:
             if next_pos not in path:
                 do_search(path + [next_pos])
@@ -90,8 +94,17 @@ def get_dictionary(dictionary_file):
     """ 
     load dictionary File
     """
+    full_words, stems= set(), set()
+    
     with open(dictionary_file) as f:
-        return[w.strip().upper() for w in f ]
+        for word in f:
+            word = word.strip().upper()
+            full_words.add(word)
+            
+            for i in range(1,len(word)):
+                stems.add(word[:i])
+                
+    return full_words, stems
         
         
 def main():
